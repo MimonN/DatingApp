8902612bd3174c7096b6b6ca9ab7b0a06ec038fe
MimonN <m.a.nedilsky@gmail.com>
2022-07-20 11:31:17 -0400

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from './_modals/user';
+import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
 
 @Component({
@@ -15,6 +15,7 @@ export class AppComponent implements OnInit {
   constructor(private accountService: AccountService) {}
 
   ngOnInit() {
+    this.setCurrentUser();
   }
 
   setCurrentUser() {
